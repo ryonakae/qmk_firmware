fbf4f22352c296048984d28f2d8c96cdac189fc6
Ryo Nakae <ryo.n.1129@gmail.com>
2019-06-11 12:51:04 +0900

    define TAPPING_TERM and IGNORE_MOD_TAP_INTERRUPT

--- keyboards/crkbd/keymaps/ryonakae/config.h
+++ keyboards/crkbd/keymaps/ryonakae/config.h
@@ -33,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 // #define TAPPING_FORCE_HOLD
-// #define TAPPING_TERM 100
-#define PERMISSIVE_HOLD
+#define TAPPING_TERM 180
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
