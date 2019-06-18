e85ebd51cfc0c826613bf816b0e77ddf506c76ef
Ryo Nakae <ryo.n.1129@gmail.com>
2019-06-18 12:55:03 +0900

    update keymap

--- keyboards/nomu30/keymaps/ryonakae/rules.mk
+++ keyboards/nomu30/keymaps/ryonakae/rules.mk
@@ -2,6 +2,6 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = lite       # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE =  no       # Mouse keys(+4700)
 TAP_DANCE_ENABLE = no
