4758c4c13e35cdd99e275c049c21722e7b14a760
Ryo Nakae <ryo.n.1129@gmail.com>
2019-08-14 23:54:52 +0900

    update keymap and fix error

--- keyboards/split_ask/config.h
+++ keyboards/split_ask/config.h
@@ -24,12 +24,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Ryo Nakae
-#define PRODUCT         split-ask
-#define DESCRIPTION     A custom keyboard
+#define PRODUCT         Split ASK
+#define DESCRIPTION     Split 60% Apple Standard Keyboard (M0116)
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
@@ -41,8 +41,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+/* left side */
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
+/* right side */
+#define MATRIX_ROW_PINS_RIGHT { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS_RIGHT { F4, F5, F6, F7, B1, B3, B2, B6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
@@ -51,7 +55,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
+#define SOFT_SERIAL_PIN D2 // or D1, D2, D3, E6
+#define SPLIT_HAND_PIN D0
+#define MASTER_LEFT
+// #define EE_HANDS
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
